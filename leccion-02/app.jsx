c310c14b47d6d7851d06233c46d793b499c8dd35
prosfp <110301233+prosfp@users.noreply.github.com>
2024-04-10 09:59:40 +0200

    inicial

---
+++ leccion-02/app.jsx
@@ -0,0 +1,12 @@
+// App.jsx
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ leccion-02/index.jsx
@@ -0,0 +1,8 @@
+// index.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+import "../index.css";
+
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<App />);
